e8ac7d53df9946a938dd18b7e5ea1d3b8fb7a6ad
Vladyslav Kosilov <wlad@wlad.com.ua>
2015-09-12 01:09:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ cp qitsimple /usr/sbin/qitsimple
 chmod +x /usr/sbin/qitsimple
 cp init /etc/init.d/qitsimple
 chmod +x /etc/init.d/qitsimple
-update-rc.d -f qitsimple default
+update-rc.d -f qitsimple enable
